aa84e280bcad6435b0bb7c15d5eba955ec3b2630
CodeRaven <youmbincbovan@gmail.com>
2025-12-28 19:21:47 +0100

    Rename structs.h to color.h

--- include/Core/Color.h
+++ include/Core/Color.h
@@ -1,22 +1,11 @@
 #pragma once
 #include <cstdint>
 
-// struct Point2D
-// {
-//     float x;
-//     float y;
-
-//     // Default and Params Constructor
-//     Point2D(float xValue = 0.0f, float yValue = 0.0f) : x(xValue), y(yValue) {}
-
-//     Point2D operator+(const Point2D& other) { return Point2D(x + other.x, y + other.y); }
-// };
-
 struct Color
 {
     uint8_t red;
-    uint8_t blue;
     uint8_t green;
+    uint8_t blue;
     uint8_t alpha;
 
     // Default Constructor (white color)
@@ -31,7 +20,8 @@ struct Color
     static Color Blue() { return Color(0, 0, 255); }
     static Color Green() { return Color(0, 255, 0); }
     static Color Yellow() { return Color(255, 255, 0); }
-    static Color Silver() { return Color(192, 192, 192); }
+    static Color LightGray() { return Color(205, 193, 180); }
+    static Color Gainsboro() { return Color(220, 220, 220); }
     static Color Black() { return Color(0, 0, 0, 0); }
     static Color TomatoOrange() { return Color(255, 99, 71); }
 };
