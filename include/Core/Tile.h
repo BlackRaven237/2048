ef0fa8391b41d90698dc6163cd1d7ee3647e9e9f
CodeRaven <youmbincbovan@gmail.com>
2025-12-29 16:50:06 +0100

    Adding new methods to Tile Class

--- include/Core/Tile.h
+++ include/Core/Tile.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Color.h"
 #include <random>
+#include <SDL3/SDL.h>
+#include <iostream>
 
 struct Coord2D {
     float x;
@@ -13,21 +15,30 @@ enum class TileType {
     TILE
 };
 
+enum class Key {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
 class Tile {
     int mIndex;
     bool mIsOccupied;
-    float size;
-    
     std::mt19937 mRandomGenerator;
+    
 public:
     Coord2D position;
+    float size;
     Color color;
+
     Tile(int index, float size);
     Tile(const Tile& other);
 
-    void Move(float deltaTime);
-    void Update(float deltaTime);
-    void Render();
+    void Move(Key key);
+    void Update();
+
+    void Render(SDL_Renderer* renderer);
 private:
     int GenerateRandomIndex();
 };
