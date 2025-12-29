73fc4144b4d6339499350c7939466731ad3e33ab
CodeRaven <youmbincbovan@gmail.com>
2025-12-29 08:28:56 +0100

    Adding new private methods to GameEngine Class

--- include/Core/GameEngine.h
+++ include/Core/GameEngine.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "Grid.h"
+#include "Graphics/Window.h"
 
 class GameEngine {
+    Window mWindow;
     Grid mGrid;
     bool mRunning;
 public:
-    GameEngine();
+    GameEngine(const std::string& title, float width, float height);
     ~GameEngine();
     bool Initialize();
     void Run();
     void ShutDown();
 private:
     void HandleEvents();
-    void HandleInputs();
+    void HandleInputs(SDL_Keycode key);
     void Update();
-    void Render();
+    void Render(SDL_Renderer* renderer);
 };
