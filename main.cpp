1d5cc4c3a7ee61d09e69318db8d60cf1882f910a
CodeRaven <youmbincbovan@gmail.com>
2025-12-30 13:25:45 +0100

    Adding a Time Seeder to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "include/Core/GameEngine.h"
 
 int main (int argc, char* argv[]) {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    
     std::cout << " ====== Welcome to my 2048 puzzle Game ====== " << std::endl;
     GameEngine game("🧩2048", 800, 640);
     if(!game.Initialize()) {
