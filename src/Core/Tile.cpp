2d190ae9bbe0fa7fd5d38e18cdaf28cc22288e75
CodeRaven <youmbincbovan@gmail.com>
2025-12-30 08:44:22 +0100

    Implementing the methods of Cell Class

--- src/Core/Tile.cpp
+++ src/Core/Tile.cpp
@@ -1,11 +1,45 @@
 #include "Core/Tile.h"
 
-Tile::Tile(int index, float size) : mIndex(index), size(size), color(Color::LightGray()) {}
+Tile::Tile(Coord2D pos, int rowValue, int columnValue, float size) : 
+        position(pos), row(rowValue), column(columnValue), 
+        mIndex(CalculateTileIndex()), size(size), color(Color::Green()) {}
 
-Tile::Tile(const Tile& other) : mIndex(other.mIndex), size(other.size), color(other.color) {}
+Tile::Tile(const Tile& other) : 
+    position(other.position), row(other.row), column(other.column), 
+    mIndex(other.mIndex), size(other.size), color(other.color) {}
+
+// void Tile::Update(std::vector<Tile>& cells) {
+
+// }
+// bool Tile::IsValidMove() {
+//     return row > 0 && row < 3 && column > 0 && column < 3;
+// }
+
+void Tile::Move(Key key) {
+    switch (key)
+    {
+        case Key::UP:
+            if(row > 0) row--;
+            break;
+        case Key::DOWN:
+            if(row < 3) row++;
+            break;
+        case Key::LEFT:
+            if(column > 0) column--;
+            break;
+        case Key::RIGHT:
+            if(column < 3) column++;
+            break;
+    }
+    mIndex = CalculateTileIndex();
+}
+
+int Tile::GenerateRandomIndex() {
+    return 10;
+}
 
 void Tile::Render(SDL_Renderer* renderer) {
-    SDL_FRect cell = {
+    SDL_FRect tile = {
         position.x,
         position.y,
         size,
@@ -13,9 +47,5 @@ void Tile::Render(SDL_Renderer* renderer) {
     };
 
     SDL_SetRenderDrawColor(renderer, color.red, color.green, color.blue, 255);
-    SDL_RenderFillRect(renderer, &cell);
-}
-
-int Tile::GenerateRandomIndex() {
-
+    SDL_RenderFillRect(renderer, &tile);
 }
