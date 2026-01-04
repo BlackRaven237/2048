9cc2c695a9c8ccde4e8855fb7f579a537ae5ec14
CodeRaven <youmbincbovan@gmail.com>
2026-01-04 22:51:44 +0100

    Ameliorating the Build system

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 chcp 65001 >nul
 
 REM ====== COMPILATION DES FICHIERS SOURCES EN FICHIER OBJETS ======
@@ -9,46 +10,79 @@ echo.
 
 REM ====== Options ======
 set CXX=clang++
-set CXXFLAGS=-O2 -g 
 set STANDARD=-std=c++20
+set CXXFLAGS=%STANDARD% -O2 -g 
 set LDFLAGS=-lSDL3
 
 REM ====== Directories ======
-set INCLUDE_DIR=include
+set INCLUDE_DIR=-Iinclude
 set SOURCES=main.cpp ^
             src\Core\*.cpp ^
             src\Graphics\*.cpp
+set OBJ_DIR=build\obj
+set BIN_DIR=build\bin
 set OUTPUT_NAME=App
-set OUTPUT_DIR=build\bin
 
-echo    =========================================
-echo     COMPILATION DES FICHIERS SOURCES (.cpp)
-echo    =========================================
+REM ------ Compiling source files to (.o) ------
+echo    ======================================================
+echo     COMPILATION DES FICHIERS SOURCES(.cpp) EN OBJETS(.o)
+echo    ======================================================
+echo.
+
+set OBJ_FILES=
+set OBJ_FILE=
+set /a OBJ_COUNT=0
+
+for /r %%f in (*.cpp) do (
+    echo    Compiling: %%~nxf
+    set OBJ_FILE="%OBJ_DIR%\%%~nf.o"
+
+    %CXX% %CXXFLAGS% %INCLUDE_DIR% -c "%%f" -o !OBJ_FILE!
+
+    if !errorlevel! neq 0 (
+        echo    [ERROR] Error at %%~nxf
+        exit /b 1
+    )
+
+    set OBJ_FILES=!OBJ_FILES! %%f
+    set /a OBJ_COUNT+=1
+)
 
 echo.
-echo    Compiling.....
+echo    [OK] Compilation has succeed.
 echo.
-REM ------ Compilation command ------
-%CXX% %STANDARD% %CXXFLAGS% %SOURCES% -I%INCLUDE_DIR% -o %OUTPUT_DIR%\%OUTPUT_NAME% %LDFLAGS%
+
+REM ------ Compiling object files (.o) ------
+echo    =====================================
+echo     COMPILATION DES FICHIERS OBJETS(.o)
+echo    =====================================
+echo.
+
+%CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %BIN_DIR%\%OUTPUT_NAME% %LDFLAGS%
 
 REM ====== Verifications ======
-if not exist "%OUTPUT_DIR%\%OUTPUT_NAME%.exe" (
+if not exist "%BIN_DIR%\%OUTPUT_NAME%.exe" (
     echo    [ERROR] No executable file found !!!
     echo    Run again
     exit /b 1
 )
-echo    [OK] Compilation has succeed
+echo    [OK] executable successfully being created.
 echo.
+
+echo    =============================================
+echo                RESUME DE COMPILATION
+echo                ---------------------
+echo.
+echo       Nombres de fichiers objets: %OBJ_COUNT%
+echo       Executable: %OUTPUT_NAME%.exe
+echo       Emplacement: %BIN_DIR%\%OUTPUT_NAME%
+echo    =============================================
 
 echo    =========================================
 echo         LANCEMENT DE L'EXECUTABLE (.exe)
 echo    =========================================
-echo.
-
 echo    Executing.....
 echo.
-echo    press enter to continue.....
-pause >nul
-echo.
 
-.\%OUTPUT_DIR%\%OUTPUT_NAME%
+.\%BIN_DIR%\%OUTPUT_NAME%
+pause >nul
