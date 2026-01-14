1dce3f26ad76cc0ce25c326fad17dbb3dd6ac2ce
CodeRaven <youmbincbovan@gmail.com>
2026-01-14 16:54:22 +0100

    Adding new features to build

--- build.bat
+++ build.bat
@@ -2,87 +2,111 @@
 setlocal EnableDelayedExpansion
 chcp 65001 >nul
 
-REM ====== COMPILATION DES FICHIERS SOURCES EN FICHIER OBJETS ======
-echo    =========================================
-echo              🏗️  BUILD SDL3 PROJECT 
-echo    =========================================
+echo    ========================================
+echo             🏠 BUILD SDL3 PROJECT 
+echo    ========================================
 echo.
 
-REM ====== Options ======
+REM ====== Options de compilation ======
 set CXX=clang++
 set STANDARD=-std=c++20
 set CXXFLAGS=%STANDARD% -O2 -g 
 set LDFLAGS=-lSDL3
 
-REM ====== Directories ======
+REM ====== Gestion des répertoires ======
 set IMGUI_DIR=external-libs/Imgui
 set IMGUI_INCLUDES=-I%IMGUI_DIR% -I%IMGUI_DIR%/backends
 set INCLUDE_DIR=-Iinclude %IMGUI_INCLUDES%
-set SOURCES=main.cpp ^
-            src\Core\*.cpp ^
-            src\Graphics\*.cpp
-set OBJ_DIR=build\obj
-set BIN_DIR=build\bin
+set BUILD_DIR=build
+set OBJ_DIR=%BUILD_DIR%\obj
+set BIN_DIR=%BUILD_DIR%\bin
 set OUTPUT_NAME=App
 
-REM ------ Vérifications des ressources ------
-echo    [1/4] 📝 Vérification du Répertoire Imgui....
+REM ====== Vérifications des ressources ======
+echo [1/3] 📝 Vérification du ressources....
 if not exist "%IMGUI_DIR%" (
-    echo        [ERROR] ❌  Répertoire Imgui non-trouvé !!!
-    echo        Pour gérer ce problème, Télécharger Dear Imgui sur https://github.com/ocornut/imgui.git 
+    echo    ❌ Répertoire Imgui non-trouvé !!!
+    echo    Pour gérer ce problème, Télécharger Dear Imgui sur https://github.com/ocornut/imgui.git 
     exit /b 1
 )
-echo        [OK] ✔️  Répertoire Imgui trouvé !!!
+echo       ✅ Répertoire Imgui trouvé !!!
 echo.
 
-REM ------ Compilation des fichiers objets (.o) ------
-echo    [2/4] 🛠️  Compilation des fichiers objets....
+if not exist "%BUILD_DIR%" (
+    echo       🎨 Création du dossier build....
+    mkdir %BUILD_DIR%
+    mkdir %OBJ_DIR%
+    mkdir %BIN_DIR%
+    echo       ✅ Répertoire Build créé !!!
+)
+
+REM ====== Compilation des fichiers objets (.o) ======
+echo [2/3] 🛠️  Compilation des fichiers objets....
+
+if not exist "%BUILD_DIR%\modif_date.txt" (
+    echo Last modification dates >> %BUILD_DIR%\modif_date.txt
+)
+
 set OBJ_FILES=
 set OBJ_FILE=
+set LAST_MODIF_DATE=
+set CURRENT_MODIF_DATE=
+set CHECK="TRUE"
 set /a OBJ_COUNT=0
 
 for /r %%f in (*.cpp) do (
-    echo        Compiling: %%~nxf
-    set OBJ_FILE="%OBJ_DIR%\%%~nf.o"
+    @REM ====== Collecte des dates de modifications ======
+    for /f "tokens=*" %%a in ('findstr "%%~nxf" %BUILD_DIR%\modif_date.txt') do (
+        set LAST_MODIF_DATE=%%a
+        set LAST_MODIF_DATE=!LAST_MODIF_DATE:~0,16!
+    )
 
-    %CXX% %CXXFLAGS% %INCLUDE_DIR% -c "%%f" -o !OBJ_FILE!
+    if !LAST_MODIF_DATE! neq %%~tf (
+        echo       Compiling: %%~nxf
+        set OBJ_FILE="%OBJ_DIR%\%%~nf.o"
+        %CXX% %CXXFLAGS% %INCLUDE_DIR% -c "%%f" -o !OBJ_FILE!
+        set CHECK="FALSE"
+    )
 
     if !errorlevel! neq 0 (
-        echo        [ERROR] ❌  Error at %%~nxf
+        echo    ❌ Error at %%~nxf
         exit /b 1
     )
 
     set OBJ_FILES=!OBJ_FILES! %%f
     set /a OBJ_COUNT+=1
 )
-echo.
-echo        [OK] ✔️  Compilation des fichiers objets réussi.
+
+if %CHECK% equ "TRUE" (
+    echo       🙂 Aucun fichier n'a été modifié.
+)
+
+@REM ====== Mises à jour des nouvelles dates de modifications ======
+echo. > %BUILD_DIR%\modif_date.txt
+for /r %%f in (*.cpp) do (
+    set CURRENT_MODIF_DATE=%%~tf %%~nxf 
+    echo !CURRENT_MODIF_DATE! >> %BUILD_DIR%\modif_date.txt
+)
+echo       ✅ Compilation des fichiers objets réussi.
 echo.
 
-REM ------ Édition des liens (ld) ------
-echo    [3/4] 🔗  Édition des liens (linking)....
+REM ====== Édition des liens (ld) ======
+echo [3/3] ⛓️  Édition des liens (linking)....
 %CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %BIN_DIR%\%OUTPUT_NAME% %LDFLAGS%
 
-if not exist "%BIN_DIR%\%OUTPUT_NAME%.exe" (
-    echo        [ERROR] ❌  Pas d'éxecutable trouvé !!!
-    echo        Relancer le build.
-    exit /b 1
-)
-echo        [OK] ✔️  Éxecutable créer avec succès.
+echo       ✅ Éxecutable créer avec succès.
 echo.
 
-echo    =============================================
-echo                RESUME DE COMPILATION
-echo                ---------------------
+echo    =================================================
+echo                   RESUME DE COMPILATION
+echo                   ---------------------
 echo.
-echo       Nombres de fichiers objets: %OBJ_COUNT%
-echo       Executable: %OUTPUT_NAME%.exe
-echo       Emplacement: %BIN_DIR%\%OUTPUT_NAME%
-echo    =============================================
+echo      Nombres de fichiers objets: %OBJ_COUNT%
+echo      Executable: %OUTPUT_NAME%.exe
+echo      Emplacement: %BIN_DIR%\%OUTPUT_NAME%
+echo      Pour lancer utiliser run.bat ou 
+echo      %BIN_DIR%\%OUTPUT_NAME%.exe
+echo    =================================================
 
-echo.
-echo    [4/4] 🚀  Lancement de l'éxecutable....
-echo.
-
-.\%BIN_DIR%\%OUTPUT_NAME%
 pause >nul
+exit /b 1
