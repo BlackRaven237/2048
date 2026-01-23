14e9cdb2b28b6e38af285c9c734db4baba5609d7
CodeRaven <youmbincbovan@gmail.com>
2026-01-23 14:33:05 +0100

    Renaming Point to Vector2D

--- include/Helpers/structs.h
+++ include/Helpers/structs.h
@@ -9,12 +9,12 @@ enum class Key {
     NONE
 };
 
-struct Point {
+struct Vector2D {
     float x;
     float y;
-    Point(float xValue = 0.0f, float yValue = 0.0f) : x(xValue), y(yValue) {}
-    Point(const Point& other) : x(other.x), y(other.y) {}
-    Point& operator=(const Point& other) {
+    Vector2D(float xValue = 0.0f, float yValue = 0.0f) : x(xValue), y(yValue) {}
+    Vector2D(const Vector2D& other) : x(other.x), y(other.y) {}
+    Vector2D& operator=(const Vector2D& other) {
         if (this == &other) return *this;
         this->x = other.x;
         this->y = other.y;
