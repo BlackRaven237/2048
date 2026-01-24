82db52c2f66505e019125ebd6741bf5eb89c89d2
CodeRaven <youmbincbovan@gmail.com>
2026-01-24 22:34:38 +0100

    Adding a Font member to Window class and implementing new methods

--- include/Graphics/Window.h
+++ include/Graphics/Window.h
@@ -2,6 +2,7 @@
 #define WINDOW_H
 
 #include <string>
+#include "Font.h"
 #include "Renderer.h"
 
 class Window {
@@ -10,15 +11,22 @@ class Window {
     float mHeight;
     SDL_Window* mWindow;
     Renderer* mRenderer;
+    Font* mFont;
     bool mIsInitialized;
+
 public:
     Window(const std::string& title, float width, float height);
     ~Window();
+
     bool Initialize();
     void Clear();
     void Present();
+
     SDL_Window* GetWindow() const;
     SDL_Renderer* GetRenderer() const;
+    Font* GetFont() const;
+
+    void RenderText(const std::string& text, float x, float y, const Color& color);
 };
 
 #endif
--- src/Graphics/Window.cpp
+++ src/Graphics/Window.cpp
@@ -2,9 +2,16 @@
 
 Window::Window(const std::string& title, float width, float height) : 
     mTitle(title), mWidth(width), mHeight(height),
-    mWindow(nullptr), mRenderer(new Renderer), mIsInitialized(false) {}
+    mWindow(nullptr), mRenderer(new Renderer), mFont(new Font),
+    mIsInitialized(false) {}
 
 Window::~Window() {
+    if(mFont) {
+        mFont->CloseFont();
+        delete mFont;
+        mFont = nullptr;
+    }
+
     if (mRenderer) {
         mRenderer->DestroyRenderer();
         delete mRenderer;
@@ -18,12 +25,12 @@ Window::~Window() {
 
     SDL_Quit();
     mIsInitialized = false;
-    SDL_Log("Window has successfully been closed");
+    SDL_Log("✅ Window successfully closed");
 }
 
 bool Window::Initialize() {
     if(!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("SDL_Init() error : %s\n", SDL_GetError());
+        SDL_Log("❌ SDL_Init() error : %s\n", SDL_GetError());
         return false;
     }
 
@@ -35,19 +42,23 @@ bool Window::Initialize() {
     );
 
     if(!mWindow) {
-        SDL_Log("Window couldn't created: %s\n", SDL_GetError());
+        SDL_Log("❌ Couldn't create window: %s\n", SDL_GetError());
         SDL_Quit();
         return false;
     }
 
     if(!mRenderer->CreateRenderer(mWindow)) {
-        SDL_Log("Renderer couldn't created: %s\n", SDL_GetError());
+        SDL_Log("❌ Couldn't create renderer: %s\n", SDL_GetError());
         SDL_DestroyWindow(mWindow);
         SDL_Quit();
     }
 
+    if(!mFont->InitializeFont("assets/fonts/Roboto-Bold.ttf")) {
+        SDL_Log("❌ Couldn't load TTF: %s\n", SDL_GetError());
+    }
+
     mIsInitialized = true;
-    SDL_Log("Window has been successfully initialized");
+    SDL_Log("✅ Window successfully initialized");
     return true;
 }
 
@@ -63,6 +74,30 @@ void Window::Present() {
 SDL_Window* Window::GetWindow() const {
     return mWindow;
 }
+
 SDL_Renderer* Window::GetRenderer() const {
     return mRenderer->GetRenderer();
+}
+
+Font* Window::GetFont() const {
+    return mFont;
+}
+
+void Window::RenderText(const std::string& text, float x, float y, const Color& color) {
+    SDL_Texture* texture = mFont->LoadText(mRenderer->GetRenderer(), text, color);
+
+    if(!texture) {
+        SDL_Log("❌ Couldn't load texture %s\n", SDL_GetError());
+        return;
+    }
+
+    SDL_FRect dstRect = { x, y, 
+        static_cast<float>(mFont->GetSurface()->w), 
+        static_cast<float>(mFont->GetSurface()->h)
+    };
+
+    SDL_RenderTexture(mRenderer->GetRenderer(), texture, NULL, &dstRect);
+    mRenderer->Present();
+
+    if(texture) SDL_DestroyTexture(texture);
 }
