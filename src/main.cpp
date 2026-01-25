22d7228a0d0df13084c9572ee877d31ab245f49d
CodeRaven <youmbincbovan@gmail.com>
2026-01-25 06:57:57 +0100

    Allocating dynamically space for game in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "../include/Core/GameEngine.h"
-#include <cstdlib>
 #include <ctime>
 
 int main (int argc, char* argv[]) {
@@ -8,10 +7,18 @@ int main (int argc, char* argv[]) {
     srand(static_cast<unsigned int>(time(nullptr)));
 
     std::cout << " ====== Welcome to my 2048 puzzle Game ====== " << std::endl;
-    GameEngine game("🧩2048", 800, 640);
-    if(!game.Initialize()) {
-        std::cerr << "Game Engine couldn't launch" << std::endl;
+    GameEngine* Game = new GameEngine("🧩2048", 800, 640);
+    if(!Game->Initialize()) {
+        std::cerr << "❌ Couldn't launch game" << std::endl;
     }
-    game.Run();
+
+    Game->Run();
+
+    if (Game) {
+        std::cout << "👋 Thanks for playing" << std::endl;
+        delete Game;
+        Game = nullptr;
+    }
+
     return 0;
 }
