8d93a8e2c860b148d9da0579e81fd780c31fc3df
CodeRaven <youmbincbovan@gmail.com>
2026-01-25 21:13:55 +0100

    Adding a verification step to checkout SDL3 libraries

--- build.bat
+++ build.bat
@@ -14,46 +14,59 @@ set CXXFLAGS=%STANDARD% -Wall -Wextra -O2 -g
 
 REM ====== Gestion des répertoires ======
 
-REM ==== Imgui ====
-set IMGUI_DIR=external-libs\Imgui
-set IMGUI_INCLUDES=-I%IMGUI_DIR% -I%IMGUI_DIR%\backends
-set INCLUDE_DIR=-Iinclude %IMGUI_INCLUDES%
-
-REM ==== SDL3 ====
-set SDL3_DIR=external-libs\SDL3
-set SDL3_LIBS=-lSDL3 -lSDL3_ttf
-
 REM ==== Build ====
 set BUILD_DIR=build
+set LIBS_DIR=%BUILD_DIR%\libs
 set OBJ_DIR=%BUILD_DIR%\obj
 set BIN_DIR=%BUILD_DIR%\bin
-set OUTPUT_NAME=App
+set OUTPUT_NAME=2048_Game
+
+REM ==== Imgui ====
+set IMGUI_DIR=externals\Imgui
+set IMGUI_LIB=
+set IMGUI_INCLUDES=-I%IMGUI_DIR% -I%IMGUI_DIR%\backends
+
+REM ==== SDL3 ====
+set SDL3_DIR=C:\msys64\ucrt64\include
+set SDL3_LIBS=-lSDL3 -lSDL3_ttf
+
+REM ==== Includes ====
+set INCLUDE_DIR=-Iinclude %IMGUI_INCLUDES%
 
 REM ====== Vérifications des ressources ======
-echo [1/4] 📝 Vérification des ressources....
+echo [1/5] 📝 Vérification des ressources....
 if not exist "%IMGUI_DIR%" (
     echo       ❌ Répertoire Imgui introuvable !!!
     echo       Pour gérer ce problème, Télécharger Dear Imgui sur https://github.com/ocornut/imgui.git 
-    rem exit /b 1
+    exit /b 1
 )
 echo       ✅ Répertoire Imgui trouvé !!!
 
-if not exist "C:\msys64\ucrt64\include\SDL3" (
+if not exist "%SDL3_DIR%\SDL3" (
     echo       ❌ Répertoire SDL3 introuvable !!!
     exit /b 1
 )
 echo       ✅ Répertoire SDL3 trouvé !!!
 echo.
 
-echo [2/4] 🎨 Préparation du répertoire Build....
-if not exist "%OBJ_DIR%" mkdir %OBJ_DIR%
-if not exist "%BIN_DIR%" mkdir %BIN_DIR%
+echo [2/5] 🎨 Préparation du répertoire Build....
+if not exist "%OBJ_DIR%" mkdir -p %OBJ_DIR%
+if not exist "%BIN_DIR%" mkdir -p %BIN_DIR%
+if not exist "%LIBS_DIR%" mkdir -p %LIBS_DIR%
 if not exist "%BUILD_DIR%\modif_date.txt" echo Last modification dates >> %BUILD_DIR%\modif_date.txt
 echo       ✅ Répertoire Build prêt !!!
 echo.
 
+echo [3/5] 📝 Vérification des bibliothèques....
+if not exist "%LIBS_DIR%\SDL3.dll" if not exist "%LIBS_DIR%\SDL3_ttf.dll" (
+    echo       ⛔ Aucune bibliothèque SDL3 trouvé !!!
+    exit /b 1
+)
+echo       ⛔ Bibliothèques SDL3 trouvé !!!
+echo.
+
 REM ====== Compilation des fichiers objets (.o) ======
-echo [3/4] 🛠️  Compilation des fichiers objets....
+echo [4/5] 🛠️  Compilation des fichiers objets....
 
 set OBJ_FILES=
 set OBJ_FILE=
@@ -62,7 +75,7 @@ set CURRENT_MODIF_DATE=
 set CHECK="TRUE"
 set /a OBJ_COUNT=0
 
-for /r %%f in (*.cpp) do (
+for /r %%f in (src\*.cpp src\Core\*.cpp src\Graphics\*.cpp) do (
     @REM ====== Collecte des dates de modifications ======
     for /f "tokens=*" %%a in ('findstr "%%~nxf" %BUILD_DIR%\modif_date.txt') do (
         set LAST_MODIF_DATE=%%a
@@ -99,11 +112,11 @@ echo       ✅ Compilation des fichiers objets réussi.
 echo.
 
 REM ====== Éditeur de liens (ld) ======
-echo [4/4] ⛓️  Éditeur de liens (linking)....
-%CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %BIN_DIR%\%OUTPUT_NAME% -L%SDL3_DIR% %SDL3_LIBS%
+echo [5/5] ⛓️  Éditeur de liens (linking)....
+%CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %BIN_DIR%\%OUTPUT_NAME% -L%LIBS_DIR% %SDL3_LIBS% %IMGUI_LIB%
 
 if not exist "%BIN_DIR%\%OUTPUT_NAME%.exe" (
-    echo       ❌ Aucun éxecutable créer !!!
+    echo       ⛔ Aucun éxecutable créer !!!
     echo       Erreur générer au niveau du linker
     exit /b 1
 ) else (
