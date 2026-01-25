43e7c84ecb808dfefcb66ddf51e7abf33755acb4
CodeRaven <youmbincbovan@gmail.com>
2026-01-25 21:47:01 +0100

    Updating script in order to create an Imgui Library

--- build.bat
+++ build.bat
@@ -19,11 +19,11 @@ set BUILD_DIR=build
 set LIBS_DIR=%BUILD_DIR%\libs
 set OBJ_DIR=%BUILD_DIR%\obj
 set BIN_DIR=%BUILD_DIR%\bin
-set OUTPUT_NAME=2048_Game
+set OUTPUT_NAME=%BIN_DIR%\Game
 
 REM ==== Imgui ====
 set IMGUI_DIR=externals\Imgui
-set IMGUI_LIB=
+set IMGUI_LIB=-lImgui
 set IMGUI_INCLUDES=-I%IMGUI_DIR% -I%IMGUI_DIR%\backends
 
 REM ==== SDL3 ====
@@ -33,8 +33,12 @@ set SDL3_LIBS=-lSDL3 -lSDL3_ttf
 REM ==== Includes ====
 set INCLUDE_DIR=-Iinclude %IMGUI_INCLUDES%
 
-REM ====== Vérifications des ressources ======
-echo [1/5] 📝 Vérification des ressources....
+REM ==== Script variables ====
+set OBJ_FILES=
+set OBJ_FILE=
+set /a OBJ_COUNT=0
+
+echo [1/5] 📝 Vérifications des ressources....
 if not exist "%IMGUI_DIR%" (
     echo       ❌ Répertoire Imgui introuvable !!!
     echo       Pour gérer ce problème, Télécharger Dear Imgui sur https://github.com/ocornut/imgui.git 
@@ -62,18 +66,41 @@ if not exist "%LIBS_DIR%\SDL3.dll" if not exist "%LIBS_DIR%\SDL3_ttf.dll" (
     echo       ⛔ Aucune bibliothèque SDL3 trouvé !!!
     exit /b 1
 )
-echo       ⛔ Bibliothèques SDL3 trouvé !!!
+echo       ✅ Bibliothèques SDL3 trouvé !!!
+
+if not exist "%LIBS_DIR%\libImgui.a" (
+    echo       ⏳ Création d'une bibliothèque Imgui !!!
+    REM ==== Compilation des fichiers sources imgui en fichiers objets
+    for /r %%f in (%IMGUI_DIR%\*.cpp %IMGUI_DIR%\backends\*.cpp) do (
+        echo        Compilation: %%~nxf
+        set OBJ_FILE="%OBJ_DIR%\%%~nf.o"
+        %CXX% %CXXFLAGS% %IMGUI_INCLUDES% -c "%%f" -o !OBJ_FILE!
+
+        if !errorlevel! neq 0 (
+            echo       ❌ Erreur générer: %%~nxf
+            echo       ❌ Bibliothèque Imgui créer avec succès !!!
+            exit /b 1
+        )
+
+        set OBJ_FILES=!OBJ_FILES! %%f
+        set /a OBJ_COUNT+=1
+    )
+
+    ar rcs %LIBS_DIR%\libImgui.a %OBJ_FILES%
+    echo       ✅ Bibliothèque Imgui créer avec succès !!!
+
+    REM ==== Reset variables ====
+    set OBJ_FILES=
+    set OBJ_FILE=
+)
+echo       ✅ Bibliothèque Imgui trouvé !!!
 echo.
 
-REM ====== Compilation des fichiers objets (.o) ======
-echo [4/5] 🛠️  Compilation des fichiers objets....
-
-set OBJ_FILES=
-set OBJ_FILE=
+echo [4/5] 🛠️  Compilation des fichiers sources en objets....
 set LAST_MODIF_DATE=
 set CURRENT_MODIF_DATE=
 set CHECK="TRUE"
-set /a OBJ_COUNT=0
+
 
 for /r %%f in (src\*.cpp src\Core\*.cpp src\Graphics\*.cpp) do (
     @REM ====== Collecte des dates de modifications ======
@@ -83,14 +110,14 @@ for /r %%f in (src\*.cpp src\Core\*.cpp src\Graphics\*.cpp) do (
     )
 
     if !LAST_MODIF_DATE! neq %%~tf (
-        echo       Compiling: %%~nxf
+        echo       Compilation: %%~nxf
         set OBJ_FILE="%OBJ_DIR%\%%~nf.o"
         %CXX% %CXXFLAGS% %INCLUDE_DIR% -c "%%f" -o !OBJ_FILE!
         set CHECK="FALSE"
     )
 
     if !errorlevel! neq 0 (
-        echo    ❌ Error at %%~nxf
+        echo       ❌ Erreur générer: %%~nxf
         exit /b 1
     )
 
@@ -111,18 +138,18 @@ for /r %%f in (*.cpp) do (
 echo       ✅ Compilation des fichiers objets réussi.
 echo.
 
-REM ====== Éditeur de liens (ld) ======
-echo [5/5] ⛓️  Éditeur de liens (linking)....
-%CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %BIN_DIR%\%OUTPUT_NAME% -L%LIBS_DIR% %SDL3_LIBS% %IMGUI_LIB%
 
-if not exist "%BIN_DIR%\%OUTPUT_NAME%.exe" (
+echo [5/5] ⛓️  Éditeur de liens (linking)....
+%CXX% %CXXFLAGS% %OBJ_FILES% %INCLUDE_DIR% -o %OUTPUT_NAME% -L%LIBS_DIR% %SDL3_LIBS% %IMGUI_LIB%
+
+if not exist "%OUTPUT_NAME%.exe" (
     echo       ⛔ Aucun éxecutable créer !!!
     echo       Erreur générer au niveau du linker
     exit /b 1
 ) else (
     echo       ✅ Éxecutable créer avec succès.
+    echo.
 )
-echo.
 
 echo    =================================================
 echo                   RESUME DE COMPILATION
@@ -130,9 +157,8 @@ echo                   ---------------------
 echo.
 echo      Nombres de fichiers objets: %OBJ_COUNT%
 echo      Executable: %OUTPUT_NAME%.exe
-echo      Emplacement: %BIN_DIR%\%OUTPUT_NAME%
-echo      Pour lancer utiliser run.bat ou 
-echo      %BIN_DIR%\%OUTPUT_NAME%.exe
+echo      Emplacement: %OUTPUT_NAME%
+echo      Prochaine étape: run.bat ou %OUTPUT_NAME%.exe
 echo    =================================================
 
 pause >nul
