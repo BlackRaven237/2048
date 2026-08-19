64f83dc33a569e16e54461df587b6ad505b67447
Youmbi Bovan <youmbincbovan@gmail.com>
2026-08-19 13:23:43 +0100

    style: Removing unnecessary spaces in Vector2D.h

--- include/utils/Vector2D.h
+++ include/utils/Vector2D.h
@@ -2,31 +2,19 @@
 
 #include <cmath>
 struct Vector2D {
-    float x;
-    float y;
+    float x = 0.0f;
+    float y = 0.0f;
 
-    Vector2D(float xValue = 0.0f, float yValue = 0.0f) : x(xValue), y(yValue) {}
+    Vector2D(float x, float y) : x(x), y(y) {}
     Vector2D(const Vector2D& other) : x(other.x), y(other.y) {}
+
+    Vector2D operator+(const Vector2D& other) { return Vector2D(x + other.x, y + other.y); }
+    Vector2D operator-(const Vector2D& other) { return Vector2D(x - other.x, y - other.y); }
+    Vector2D operator*(float scalar) { return Vector2D(x * scalar, y * scalar); }
     Vector2D& operator=(const Vector2D& other) {
         if (this == &other) return *this;
         this->x = other.x;
         this->y = other.y;
         return *this;
-    }
-
-    Vector2D operator+(const Vector2D& other) {
-        float dx = x + other.x;
-        float dy = y + other.y;
-        return Vector2D(dx, dy);
-    }
-
-    Vector2D operator-(const Vector2D& other) {
-        float dx = x - other.x;
-        float dy = y - other.y;
-        return Vector2D(dx, dy);
-    }
-
-    Vector2D operator*(float scalar) {
-        return Vector2D(x * scalar, y * scalar);
     }
 };
