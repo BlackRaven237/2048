14a11a7134fadb7d5c7f411a2925da8cb5d27de7
Youmbi Bovan <youmbincbovan@gmail.com>
2026-08-24 18:04:35 +0100

    refactor: Enhancing code format

--- src/entities/Tile.cpp
+++ src/entities/Tile.cpp
@@ -23,31 +23,6 @@ void Tile::move(Vector2D targetPosition, float deltaTime) {
     }
 }
 
-Color Tile::GetTextColor() const {
-    // Dark grey text for '2' and '4' values
-    if(m_value <= 4) return Color(119, 110, 101);
-
-    // off-white for remaining values
-    return Color(249, 246, 242);
-}
-
-Color Tile::GetColorBasedOnValue() const {
-    switch (m_value) {
-        case 2: return Color(238, 228, 218);
-        case 4: return Color(237, 224, 200);
-        case 8: return Color(242, 177, 121);
-        case 16: return Color(245, 149, 99);
-        case 32: return Color(246, 124, 95);
-        case 64: return Color(246, 94, 59);
-        case 128: return Color(237, 207, 114);
-        case 256: return Color(237, 204, 97);
-        case 512: return Color(237, 200, 80);
-        case 1024: return Color(237, 197, 63);
-        case 2048: return Color(237, 194, 46);
-        default: return Color(199, 110, 101);
-    }
-}
-
 void Tile::render(SDL_Renderer* renderer) {
     float drawSize = size * scale;
     float offset = (size - drawSize) / 2;
@@ -65,6 +40,23 @@ void Tile::render(SDL_Renderer* renderer) {
     SDL_RenderFillRect(renderer, &tile);
 
     renderValue(renderer);
+}
+
+Color Tile::GetColorBasedOnValue() const {
+    switch (m_value) {
+        case 2: return Color(238, 228, 218);
+        case 4: return Color(237, 224, 200);
+        case 8: return Color(242, 177, 121);
+        case 16: return Color(245, 149, 99);
+        case 32: return Color(246, 124, 95);
+        case 64: return Color(246, 94, 59);
+        case 128: return Color(237, 207, 114);
+        case 256: return Color(237, 204, 97);
+        case 512: return Color(237, 200, 80);
+        case 1024: return Color(237, 197, 63);
+        case 2048: return Color(237, 194, 46);
+        default: return Color(199, 110, 101);
+    }
 }
 
 void Tile::renderValue(SDL_Renderer* renderer) {
@@ -90,8 +82,15 @@ void Tile::renderValue(SDL_Renderer* renderer) {
     dstRect.x = position.x + (size - dstRect.w) / 2.0f;
     dstRect.y = position.y + (size - dstRect.h) / 2.0f;
 
-
     SDL_RenderTexture(renderer, texture, NULL, &dstRect);
     SDL_DestroySurface(surface);
     SDL_DestroyTexture(texture);
+}
+
+Color Tile::GetTextColor() const {
+    // Dark grey text for '2' and '4' values
+    if(m_value <= 4) return Color(119, 110, 101);
+
+    // off-white for remaining values
+    return Color(249, 246, 242);
 }
